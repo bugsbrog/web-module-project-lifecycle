be5654a881c1e4fb1d12dfb9d848ff21bce4771a
bugsbrog <bugsbrog@gmail.com>
2021-11-30 13:14:29 -0700

    Worked on state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    userData: [],
+    currentUser: "bugsbrog",
+    followers: []
+  }
+
   render() {
     return(<div>
       Github Card
