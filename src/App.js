b3897427b1bf1d4b16905a1d72a669a0a0868676
bugsbrog <bugsbrog@gmail.com>
2021-11-30 13:39:00 -0700

    Worked on handleSubmit and render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import User from './components/User';
+import Follower from './components/User';
 
 class App extends React.Component {
   state = {
@@ -18,12 +20,34 @@ class App extends React.Component {
         })
         .catch(err => {
           console.error(err);
+        });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+        .then(res => {
+            this.setState({
+              ...this.state,
+              userData: res.data
+            })
+        })
+        .catch(err => {
+          console.error(err);
         })
   }
 
   render() {
-    return(<div>
-      Github Card
+    return(
+        <div>
+          <h1>Github Card</h1>
+          <form>
+            <input
+                value={this.state.currentUser}
+          </form>
     </div>);
   }
 }
