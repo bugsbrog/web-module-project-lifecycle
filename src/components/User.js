d578ffbd42579ffc01856a6789eece11757934d5
bugsbrog <bugsbrog@gmail.com>
2021-11-30 20:42:35 -0700

    Added more styling and ACTUALLY done

--- src/components/User.js
+++ src/components/User.js
@@ -8,10 +8,10 @@ class User extends React.Component {
                 <div className="profile">
                     <img src={this.props.user.avatar_url} alt="avatar" />
                     <div className="user-information">
-                        <h3>{this.props.user.name}</h3>
-                        <p>{this.props.user.login}</p>
-                        <p>Total Repos: {this.props.user.public_repos}</p>
-                        <p>Total Followers: {this.props.user.followers}</p>
+                        <h2>{this.props.user.name}</h2>
+                        <h5>({this.props.user.login})</h5>
+                        <h3>Total Repos: {this.props.user.public_repos}</h3>
+                        <h3>Total Followers: {this.props.user.followers}</h3>
                     </div>
                 </div>
                 <FollowerList followers={this.props.followers}/>
